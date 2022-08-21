a76d05673d9984b5fde21f72e636c6176fb51938
O6lvl4 <o6lvl4@O6lvl4noMacBook-Air.local>
2022-08-21 12:32:57 +0900

    Add example flatMap code

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 )
 
 func main() {
-	names := lo.Uniq([]string{"Samuel", "Marc", "Samuel"})
-	fmt.Println(names)
+	array1 := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(array1)
+	array2 := lo.FlatMap(array1, func(element int, _ int) []int {
+		if element%2 == 0 {
+			return []int{element, element}
+		}
+		return []int{element, element, element}
+	})
+	fmt.Println(array2)
 }
